e9bae0190bcf067731f8809a03bf413162ce06a9
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-11-03 17:20:24 -0300

    Update Fila.c

--- Fila.c
+++ Fila.c
@@ -1,39 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // tamanho maximo da fila
 #define MAX 3
 
-// Tipo base dos elementos da fila 
+// Tipo base dos elementos da lista 
 typedef struct elementos {
-    char nome[50];	
+    char nome[50];
 } t_elemento;
 
+typedef struct no {
+    t_elemento dado;
+    struct no * prox;
+} t_no;
+
 typedef struct fila {
-    t_elemento vetor[MAX]; // vetor que armazena a fila
-    int inicio; // posicao do primeiro elemento
-    int fim;    // posicao do ultimo elemento
-    int quant_element;    // numero de elementos da fila
+    t_no* inicio;
+    int quant_element;
+    t_no* final;
 } t_fila;
 
-t_fila criar(){
-    t_fila fila;
 
-    fila.inicio = 0;
-    fila.fim = -1;
-    fila.quant_element = 0;
+/**
+ * Cria uma fila vazia, ou seja um no cabeca.
+ * inicializa os ponteiros ini e fim para NULL,
+ * e seta quant_element para zero.
+ * 
+ * @return no cabeca alocado e inicializado
+ */
+t_fila * criaCabeca ()
+{
+    t_fila * fila = (t_fila*) malloc(sizeof(t_fila));
+
+    if (fila) {
+        fila->inicio = fila->final = NULL;
+        fila->quant_element=0;
+    }
 
     return fila;
 }
 
-int isVazia (t_fila * fila){
-    if(fila->quant_element == 0){
-			printf("Está vazia!\n");
-			return 0;
-		}else{
-			printf("A fila não está vazia.\n");
-			return 1;
-		}
+/**
+ * Cria um novo no, aloca a sua regiao de memoria,
+ * inicializa o ponteiro prox, e retorna o ponteiro para a pilha criada.
+ * 
+ * @return No alocada e inicializada
+ */
+t_no * criaNo() {
+    t_no * no = (t_no*) malloc(sizeof(t_no));
+
+    // verifica se houve memoria suficiente para alocar
+    if (no)
+        no->prox = NULL;
+
+    return no;
 }
+
+/**
+ * Verifica se a fila esta vazia ou nao. Isto so acontece quando ela eh nula
+ * 
+ * @param fila ponteiro para a fila
+ *
+ * @return Verdadeiro (1) se a fila estiver vazia, ou falso (0) caso contrario.
+ */
+int isVazia (t_fila * fila)
+{
+    return (fila->quant_element == 0);
+}
+
+/**
+ * Insere um elemento (valor) no fim da fila.
+ * 
+ * @param fila ponteiro para a fila
+ * @param valor  elemento a ser inserido na fila
+ *
+ * @return Falso(0) se a fila estiver cheia, caso contrario, retorna Verdadeiro(1).
+ */
+int inserir (t_fila *fila, t_elemento valor) {
+    t_no *novo;
+    novo = criaNo();
+    if (novo == NULL)
+        return 0; // Erro: memoria insuficiente
+    novo->dado = valor;
+    if (isVazia(fila))
+        fila->inicio = novo;
+    else
+        (fila->final)->prox = novo;
+    fila->final = novo;
+    fila->quant_element++;
+    return 1;
+}
+
 
 int isCheia(t_fila * fila){
     if(fila->quant_element == MAX){
@@ -45,66 +102,40 @@ int isCheia(t_fila * fila){
 		}
 }
 
-int inserir (t_fila * fila, t_elemento valor)
+/**
+ * Remove um elemento do inicio da fila.
+ * 
+ * @param fila ponteiro para a fila, a fila ja deve ter sido inicializada
+ *
+ * @return o elemento removido.
+ */
+t_elemento remover (t_fila *fila)
 {
-    if (isCheia(fila))
-        return 0;
-
-    (fila->quant_element)++;
-    fila->fim = (fila->fim + 1) % MAX;
-    fila->vetor[fila->fim] = valor;
-    return 1;
-}
-
-t_elemento remover(t_fila * fila)
-{
+    t_no *aux;
     t_elemento valor = { "" } ;
     if (isVazia(fila))
-        return valor; // Erro: fila vazia
-
-    valor = fila->vetor[fila->inicio];
-    fila->vetor[fila->inicio].nome[0] = '\0';// zera, opcional
-    (fila->quant_element)--;
-    fila->inicio = (fila->inicio + 1) % MAX;
+        return valor; // Erro: fila vazia 
+    valor = (fila->inicio)->dado;
+    if (fila->inicio == fila->final)
+        fila->final = NULL;
+    aux = fila->inicio;
+    fila->inicio = (fila->inicio)->prox;
+    free(aux);
+    fila->quant_element--;
     return valor;
-}
-
-void mostraFila(t_fila * fila){
-    int i;
-
-    if (isVazia(fila)) {
-    	printf("Fila vazia\n");
-    	return;
-    }
-
-		printf("\nExibindo fila:\n");
-    printf("inicio: %d\n", fila->inicio);
-    printf("fim:    %d\n", fila->fim);
-
-    for (i = fila->inicio; i != fila->fim + 1; i = (i + 1) % MAX){
-				printf("%s\t", fila->vetor[i].nome);
-		}
-    	
 }
 
 
 int main(void) {
   t_fila filinha;
 	t_elemento nomesLista;
+	t_no umNo;
 
+	//Criando a cabeça e o nó da filinha:
+	filinha = *criaCabeca ();
+	umNo = *criaNo();
 
-	filinha = criar();//criando filinha
-
-	//isVazia(&filinha);//está vazia?
-
-	//isCheia(&filinha);//está cheia?
-
-	printf("\nDigite um nome para inserir na lista: ");
-	scanf("%s", nomesLista.nome);//input
-
-	inserir(&filinha, nomesLista);//inserindo input anterior
-
-	remover(&filinha);
+	
 
   return 0;
 }
